c5417fa361d1accde0fa1477ebc354ccb49b0515
David L.E <59020919+neldivad@users.noreply.github.com>
2022-03-01 02:57:05 +0800

    Update pgsheet.py

--- pgsheet.py
+++ pgsheet.py
@@ -20,7 +20,7 @@ def make_df(secrets):
 
 spreadsheet_id = '1jmRQJC4wQtSny-JTA3KLJ4BeVFdYU3qQanZerh_5IEU'
 
-@st.cache(hash_funcs={_cffi_backend.__CDataGCP: my_hash_func})
+@st.cache
 def make_df2():
     # gc = pygsheets.authorize(service_account_file= st.secrets["service_file_path"])
     # gc = pygsheets.authorize(service_account_file= 'gsheet-key.json')
